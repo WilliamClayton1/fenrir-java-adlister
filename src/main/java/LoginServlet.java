81923de8e61302058f0ced852fdcafd4c48ef6a3
William Clayton <william.d.clayton1@gmail.com>
2023-11-02 14:39:43 -0500

    -wc

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -16,10 +17,16 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
         if (validAttempt) {
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+
+
+
     }
 }
