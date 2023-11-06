7951150124c115388a7f6c25870b620e06534801
William Clayton <william.d.clayton1@gmail.com>
2023-11-06 11:14:34 -0600

    -wc

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -9,18 +9,13 @@ public class BeanTest {
         Album album1 = new Album();
         Album album2 = new Album();
 
-        album1.setArtist("Beyonce");
-        album1.setName("renaissance");
-        album1.setGenre("pop");
-
-        album2.setArtist("Drake");
-        album2.setName("For the Dogs");
-        album2.setGenre("Rap");
+        album1.setArtist("Drake");
+        album1.setName("For the Dogs");
+        album1.setGenre("Rap");
 
         System.out.println(album1.getArtist());
         System.out.println(album1.getName());
         System.out.println(album1.getId());
-        System.out.println(album2.getId());
 
     }
 
